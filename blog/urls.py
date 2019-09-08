4917da373a37120efb06577300e350a988907c9c
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-09-08 21:37:16 +0200

    User collection

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.collection, name='collection'),
+    path('<int:pk>/', views.collection, name='collection'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
-def collection(request):
+def collection(request, **kwargs):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    user_pk = kwargs.get('pk')
+    if user_pk:
+        posts = posts.filter(author_id=user_pk)
     return render(request, 'blog/collection.html', {'posts': posts})
