eb8f71b8dcc88e487b1f7d853d3b065b925d19c5
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-09-11 21:28:48 +0200

    Add buttons to collection if is author

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.utils import timezone
 from .models import Post
 
 def collection(request, **kwargs):
+    context = {}
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     user_pk = kwargs.get('pk')
     if user_pk:
         posts = posts.filter(author_id=user_pk)
-    return render(request, 'blog/collection.html', {'posts': posts})
+        if user_pk == request.user.pk:
+            context['is_author'] = True
+    context['posts'] = posts
+    return render(request, 'blog/collection.html', context)
