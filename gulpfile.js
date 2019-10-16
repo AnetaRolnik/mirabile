af5e9721e376a06cbbc428f60739179f2c1547ba
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-10-16 22:27:15 +0200

    Use gulp browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const plumber         = require("gulp-plumber");
 const rename          = require("gulp-rename");
 const webpack         = require("webpack");
 const csso            = require('gulp-csso');
+const browserSync     = require('browser-sync').create();
+const exec            = require('child_process').exec;
 
 gulp.task("sass", function() {
     return gulp.src("./blog/static/src/styles/main.scss")
@@ -24,22 +26,37 @@ gulp.task("sass", function() {
         }))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./blog/static/dist"))
+        .pipe(browserSync.stream({match: "**/*.css"}));
 });
 
+gulp.task('runserver', function() {
+    const proc = exec('python manage.py runserver')
+})
+  
+gulp.task('browserSync', ['runserver'], function() {
+    browserSync.init({
+        notify: false,
+        port: 8000,
+        proxy: '127.0.0.1:8000'
+    })
+});
 
 gulp.task("es6", function(cb) {
     return webpack(require("./webpack.config.js"), function(err, stats) {
         if (err) throw err;
         console.log(stats.toString());
         cb();
+        browserSync.reload();
     })
 })
 
 gulp.task("watch", function() {
     gulp.watch("./blog/static/src/styles/**/*.scss", ["sass"]);
     gulp.watch("./blog/static/src/js/**/*.js", ["es6"]);
+    gulp.watch('./blog/templates/*.html');
 });
 
 gulp.task("default", function() {
-    gulp.start(["sass", "es6",  "watch"]);
+    gulp.start(["sass", "es6", "browserSync", "watch"]);
 });
+
