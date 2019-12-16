caf78290e82623532f2eec54769ff437bb1b0d99
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-12-16 22:55:33 +0100

    Gallery

--- blog/static/src/js/components/masonry.js
+++ blog/static/src/js/components/masonry.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     const masonry = new Macy ({
-        container: '.collection .container',
+        container: '.collection',
         columns: 3,
         margin: 5,
         breakAt: {
