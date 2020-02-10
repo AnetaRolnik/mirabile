f2b863074a2038f1e1bcc990e3c4006102810bb2
AnetaRolnik <aneta.r@poczta.onet.eu>
2020-02-10 21:58:03 +0100

    Logout

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.collection, name='collection'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('post/<int:id>/delete', views.post_delete, name='post_delete'),
     path('post/<int:id>/edit', views.post_edit, name='post_edit'),
     path('login/', LoginView.as_view(template_name='blog/registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='blog/base.html'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mirabile/settings.py
+++ mirabile/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/media')
