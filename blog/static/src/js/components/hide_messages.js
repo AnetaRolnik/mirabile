7199e13baee73cb670b21a086a72d1f87a95f733
AnetaRolnik <aneta.r@poczta.onet.eu>
2020-02-17 22:25:42 +0100

    Fix messages

--- blog/static/src/js/components/hide_messages.js
+++ blog/static/src/js/components/hide_messages.js
@@ -1,15 +1,18 @@
 function hideMessages() {
     const messages = document.querySelector('.messages');
-    const close = messages.querySelectorAll('.icon-close');
 
-    setTimeout(function() {
-        messages.remove();
-    }, 5000);
+    if(messages !== null) {
+        const close = messages.querySelectorAll('.icon-close');
 
-    for(let i=0; i<close.length; i++) {
-        close[i].addEventListener('click', function() {
-            this.parentElement.remove();
-        })
+        setTimeout(function() {
+            messages.remove();
+        }, 5000);
+            
+        for(let i=0; i<close.length; i++) {
+            close[i].addEventListener('click', function() {
+                this.parentElement.remove();
+            })
+        }
     }
 }
 
